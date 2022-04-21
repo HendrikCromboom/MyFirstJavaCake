4a53ab4cf8486c00aaec876bfb6da4c7ab0b8ded
HendrikCromboom <hcromboom@gmail.com>
2022-04-21 10:47:11 +0200

    trying out some of the semantic differences between c# and java

--- src/JavaCakes/Main.java
+++ src/JavaCakes/Main.java
@@ -1,7 +1,12 @@
 package JavaCakes;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello World");
+        int[] numbers = {0, 6, 4, 3, 9, 7, 8, 5, 2, 3, 1};
+        Arrays.sort(numbers);
+        String stringNumbers = Arrays.toString(numbers);
+        System.out.println(stringNumbers+ " -> ok");
     }
 }
