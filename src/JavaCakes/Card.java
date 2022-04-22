6f705fddc9a35a788b9d70dfc34bd54e222f111d
HendrikCromboom <hcromboom@gmail.com>
2022-04-22 12:30:09 +0200

    OOP init
    Decided to use this project as I cycle trough all the dev principles in JAVA, this will be the OOP part.
    
    The purpose of the console app will be a small proof of concept card battle game.

---
+++ src/JavaCakes/Card.java
@@ -0,0 +1,13 @@
+package JavaCakes;
+
+public class Card {
+    public int health;
+    public int attack;
+    public boolean isAlive(){
+        return health > 0;
+    }
+    public boolean inHand;
+    public boolean inDeck;
+    public boolean onField;
+    public boolean inGraveyard;
+}
